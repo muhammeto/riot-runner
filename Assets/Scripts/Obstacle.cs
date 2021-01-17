34b056bfdb49d8416e74edbd999b13e81ecbf8e7
muhammeto <m.ozdmar@gmail.com>
2021-01-17 12:36:45 +0300

    Destructable wall

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -3,7 +3,7 @@ using DG.Tweening;
 public class Obstacle : MonoBehaviour
 {
     [SerializeField] private float maxHealth = 10;
-
+    [SerializeField] private GameObject destructableVersion = default;
     private float health = 3;
     private MeshRenderer meshRenderer;
 
@@ -19,6 +19,12 @@ public class Obstacle : MonoBehaviour
         meshRenderer.material.SetFloat("BlendAmount", 1f - (health / maxHealth));
         if (health <= 0f)
         {
+            GameObject destructed =  Instantiate(destructableVersion, transform.position, Quaternion.identity);
+            foreach (Rigidbody rb in destructed.GetComponentsInChildren<Rigidbody>())
+            {
+                Vector3 force = (rb.transform.position - transform.position).normalized * 200f;
+                rb.AddForce(force);
+            }
             Destroy(gameObject);
             return true;
         }
